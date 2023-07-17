5ba0e260f4e71bb755b4350c1f3eeb0e54bb9059
Blaine Venturine <blaineventurine@users.noreply.github.com>
2023-07-17 14:04:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const algoliasearch = require('algoliasearch');
 const objects = require('./sample.json');
-
-// Please replace the placeholder text below
-const client = algoliasearch('YourAppID', 'YourApiKey');
-
-const index = client.initIndex('your_index_name');
-
-index.saveObjects(objects).catch(err => {
-  console.log(err);
-});
